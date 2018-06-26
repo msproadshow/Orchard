e6eab5f67506045a7fb00c9ba370ee27a20107da
diman96 <nde96m@gmail.com>
2018-06-26 12:28:14 +0300

    Добавил рецепт конференции
    Добавил ContentDefenitions для конференции

---
+++ src/OrchardCore.Themes/TheConferenceTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Conference Theme",
+    Author = "The V Team",
+    Website = "http://Vproject.net",
+    Version = "1.0.0",
+    Description = "A theme adapted for conferences."
+)]
